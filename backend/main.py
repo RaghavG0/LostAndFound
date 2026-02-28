e2d41fc1f1ca8402c7563d583030d0b63448f820
Raghav_Gulati <f20230996@hyderabad.bits-pilani.ac.in>
2026-03-01 03:21:28 +0530

    fixed vercel errors

--- backend/main.py
+++ backend/main.py
@@ -25,11 +25,12 @@ origins = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "https://lost-and-found-bits.vercel.app",
+    "https://lost-and-found-bits.vercel.app/"
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
